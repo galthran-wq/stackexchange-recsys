e166c7593c6c681f9b4180412b8ecc846cb6b150
Raman <rommmmmka.m@gmail.com>
2024-12-20 06:57:40 +0300

    faiss-based recommendations

--- data_utils.py
+++ data_utils.py
@@ -193,3 +193,8 @@ def preprocess_data(
     # Train/Test Split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
+
+def preprocess_data_faiss(data: dict):
+    posts = data["Posts"][data["Posts"]["posttypeid"] == "1"]
+    posts['body'] = posts['body'].map(clean_html)
+    return posts
